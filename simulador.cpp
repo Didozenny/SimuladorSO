542a6b45bfeb0f40b09d1f9f7fef318b200e4ebf
Vicente Silva Vera <ve.silvavera@gmail.com>
2022-11-22 23:54:22 -0300

    join de los threads y terminado de programa

--- simulador.cpp
+++ simulador.cpp
@@ -6,20 +6,22 @@
 #include <condition_variable>
 #include <iostream>
 #include <thread>
+#include <unistd.h>
+
 
 
 using namespace std;
-
+int finish = 0;
 runQueues rq;
 int id = 0;
-int pCount = 0;
+int pCount = 0;//Cantidad de elementos siendo procesados
 mutex m;//mutex para manejar la runqueue
 mutex p;//mutex para impresiones
 condition_variable vacio;//variable de condicion para que las hebras G intenten crear nuevas hebras
 condition_variable no_vacio;//variable de condicion para evitar procesar nuevas hebras cuando no quedan hebras a procesar
 
 void work(int idHebra, int periodo, int b){
-	while(true){
+	while(!finish){
 		unique_lock<mutex> ul(m);//obtenemos mutex
 		no_vacio.wait(ul, []{return (rq.rqSize()!=0 && (rq.getActiveSize()!=0 || pCount == 0)) ? true : false; });//en caso de que este vacio espera
 		hebraT T = rq.pop();
@@ -44,12 +46,14 @@ void work(int idHebra, int periodo, int b){
 		if(!inserted)vacio.notify_one();
 		else no_vacio.notify_all();
 	}
+	vacio.notify_one();
 }
 
 void addT(int maxAdd,int a,int b){
-	while(true){
+	while(!finish){
 		unique_lock<mutex> ul(m);
-		vacio.wait(ul, []{return (!rq.rqSize() && !pCount) ? true : false; });
+		vacio.wait(ul, []{return ((!rq.rqSize() && !pCount)||finish) ? true : false; });
+		if(finish)break;
 		for (int i = 0; i < 1 + rand()%maxAdd; ++i){
 			hebraT T(id,a,b);
 			int priority = 9*T.tiempoRestante()/b;
@@ -85,7 +89,13 @@ int main(int argc, char *argv[]){
 	for (int i = 0; i < M; ++i)
 		hebraE[i] = thread(work,i, periodo, b);
 	thread hebraG(addT,max(N,M),a,b);
-	string finish;
-	getline(cin,finish);
+	while(cin>>finish and finish== 0);
+	sleep(periodo+1);
+	for (int i = 0; i < M; ++i)
+	{
+		hebraE[i].join();
+	}
+	hebraG.join();
+	cout<<"EJECUCION TERMINADA"<<endl;
 	return 0;
 }
